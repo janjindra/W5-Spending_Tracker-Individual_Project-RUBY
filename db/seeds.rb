195f89aaa6f6cb02400980f76df5576c950c192f
User <user@Users-MacBook-Pro.local>
2020-02-03 10:20:29 +0000

    more merchants and categories added

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,6 @@ require("pry-byebug")
   'active' => true,
   'logo' => 'https://cdn.iconscout.com/icon/free/png-256/tesco-282827.png'
   })
-
   @tesco.save()
 
   @lidl = Merchant.new({
@@ -20,80 +19,160 @@ require("pry-byebug")
     'active' => true,
     'logo' => 'https://cdn.iconscout.com/icon/free/png-256/lidl-282519.png'
     })
-
     @lidl.save()
 
-
-  @groceries = Tag.new({
-    'label' => 'groceries',
-    'active' => true,
-    'logo' => 'https://www.shareicon.net/data/256x256/2016/08/19/817434_food_512x512.png'
-    })
-
-    @groceries.save()
-
-    @user1 = User.new({
-      'name' => 'Jan',
-      'budget' => 1000
+    @cineworld = Merchant.new({
+      'name' => 'CineWorld',
+      'active' => true,
+      'logo' => 'https://apprecs.org/ios/images/app-icons/256/3f/508350271.jpg'
       })
+      @cineworld.save()
 
-    @user1.save()
-
-    @transaction1 = Transaction.new({
-      'merchant_id' => @tesco.id,
-      'tag_id' => @groceries.id,
-      'user_id' => @user1.id,
-      'amount' => 11.20,
-      'time' => Time.now
-      })
-
-      @transaction1.save()
-
-      @transaction2 = Transaction.new({
-        'merchant_id' => @tesco.id,
-        'tag_id' => @groceries.id,
-        'user_id' => @user1.id,
-        'amount' => 133.20,
-        'time' => Time.now
+      @boots = Merchant.new({
+        'name' => 'Boots',
+        'active' => true,
+        'logo' => 'https://is2-ssl.mzstatic.com/image/thumb/Purple123/v4/68/6f/d4/686fd4b7-bd79-714e-5607-c16706ebc221/source/256x256bb.jpg'
         })
+        @boots.save()
 
-        @transaction2.save()
 
+    @groceries = Tag.new({
+      'label' => 'groceries',
+      'active' => true,
+      'logo' => 'https://www.shareicon.net/data/256x256/2016/08/19/817434_food_512x512.png'
+      })
+      @groceries.save()
 
-        @transaction3 = Transaction.new({
-          'merchant_id' => @lidl.id,
-          'tag_id' => @groceries.id,
-          'user_id' => @user1.id,
-          'amount' => 88,
-          'time' => Time.now
+      @shopping = Tag.new({
+        'label' => 'shopping',
+        'active' => true,
+        'logo' => 'https://www.shareicon.net/data/256x256/2016/08/19/817434_food_512x512.png'
+        })
+        @shopping.save()
+
+        @restaurants = Tag.new({
+          'label' => 'restaurants',
+          'active' => true,
+          'logo' => 'https://www.shareicon.net/data/256x256/2016/08/19/817434_food_512x512.png'
           })
+          @restaurants.save()
 
-          @transaction3.save()
-
-          @transaction2.time = Time.new(2018, 10, 31)
-          @transaction2.update()
-
-          @transaction3.time = Time.new(2019, 5, 22)
-          @transaction3.update()
-
-          @transaction4 = Transaction.new({
-            'merchant_id' => @lidl.id,
-            'tag_id' => @groceries.id,
-            'user_id' => @user1.id,
-            'amount' => 1111,
-            'time' => Time.now
+          @transport = Tag.new({
+            'label' => 'transport',
+            'active' => true,
+            'logo' => 'https://www.shareicon.net/data/256x256/2016/08/19/817434_food_512x512.png'
             })
+            @transport.save()
 
-            @transaction4.save()
+            @entertainment = Tag.new({
+              'label' => 'entertainment',
+              'active' => true,
+              'logo' => 'https://www.shareicon.net/data/256x256/2016/08/19/817434_food_512x512.png'
+              })
+              @entertainment.save()
 
-            @transaction4.time = Time.new(2020, 02, 01)
-            @transaction4.update()
+              @health = Tag.new({
+                'label' => 'health',
+                'active' => true,
+                'logo' => 'https://www.shareicon.net/data/256x256/2016/08/19/817434_food_512x512.png'
+                })
+                @health.save()
 
+                @services = Tag.new({
+                  'label' => 'services',
+                  'active' => true,
+                  'logo' => 'https://www.shareicon.net/data/256x256/2016/08/19/817434_food_512x512.png'
+                  })
+                  @services.save()
 
-          Tag.all()
-          Merchant.all()
-          Transaction.all()
-          User.all()
+                  @utilities = Tag.new({
+                    'label' => 'utilities',
+                    'active' => true,
+                    'logo' => 'https://www.shareicon.net/data/256x256/2016/08/19/817434_food_512x512.png'
+                    })
+                    @utilities.save()
 
-        binding.pry
-        nil
+                    @rent = Tag.new({
+                      'label' => 'rent',
+                      'active' => true,
+                      'logo' => 'https://www.shareicon.net/data/256x256/2016/08/19/817434_food_512x512.png'
+                      })
+                      @rent.save()
+
+                      @user1 = User.new({
+                        'name' => 'Jan',
+                        'budget' => 1000
+                        })
+                        @user1.save()
+
+                        @transaction1 = Transaction.new({
+                          'merchant_id' => @tesco.id,
+                          'tag_id' => @groceries.id,
+                          'user_id' => @user1.id,
+                          'amount' => 11.20,
+                          'time' => Time.now
+                          })
+                          @transaction1.save()
+
+                          @transaction2 = Transaction.new({
+                            'merchant_id' => @tesco.id,
+                            'tag_id' => @groceries.id,
+                            'user_id' => @user1.id,
+                            'amount' => 133.20,
+                            'time' => Time.now
+                            })
+                            @transaction2.save()
+                            @transaction2.time = Time.new(2018, 10, 31)
+                            @transaction2.update()
+
+                            @transaction3 = Transaction.new({
+                              'merchant_id' => @lidl.id,
+                              'tag_id' => @groceries.id,
+                              'user_id' => @user1.id,
+                              'amount' => 88,
+                              'time' => Time.now
+                              })
+                              @transaction3.save()
+                              @transaction3.time = Time.new(2019, 5, 22)
+                              @transaction3.update()
+
+                              @transaction4 = Transaction.new({
+                                'merchant_id' => @lidl.id,
+                                'tag_id' => @groceries.id,
+                                'user_id' => @user1.id,
+                                'amount' => 1111,
+                                'time' => Time.now
+                                })
+                                @transaction4.save()
+                                @transaction4.time = Time.new(2020, 02, 01)
+                                @transaction4.update()
+
+                                @transaction5 = Transaction.new({
+                                  'merchant_id' => @boots.id,
+                                  'tag_id' => @health.id,
+                                  'user_id' => @user1.id,
+                                  'amount' => 33,
+                                  'time' => Time.now
+                                  })
+                                  @transaction5.save()
+                                  @transaction5.time = Time.new(2020, 01, 05)
+                                  @transaction5.update()
+
+                                  @transaction6 = Transaction.new({
+                                    'merchant_id' => @cineworld.id,
+                                    'tag_id' => @entertainment.id,
+                                    'user_id' => @user1.id,
+                                    'amount' => 66,
+                                    'time' => Time.now
+                                    })
+                                    @transaction6.save()
+                                    @transaction6.time = Time.new(2019, 04, 22)
+                                    @transaction6.update()
+
+                                Tag.all()
+                                Merchant.all()
+                                Transaction.all()
+                                User.all()
+
+                                binding.pry
+                                nil
